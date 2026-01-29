4dfb9fb58b79008d7bbc19ea26eaec8da203d6b3
alized1995 <noman@maxoutsms.com>
2026-01-29 10:11:13 -0500

    some initial DB creation

---
+++ backend/WorldCities.Api/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WorldCities.Api.Models;
+
+namespace WorldCities.Api.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<City> Cities => Set<City>();
+    }
+}
---
+++ backend/WorldCities.Api/Models/City.cs
@@ -0,0 +1,10 @@
+﻿namespace WorldCities.Api.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public required string Name { get; set; }
+        public decimal Lat { get; set; }
+        public decimal Lon { get; set; }
+    }
+}
